5672c6cd834b1f811841748af09d26cc2b5340ad
Antoine Auger <Ardyn142@gmail.com>
2024-05-14 10:40:56 -0400

    load BD

--- app/src/main/java/com/example/cinebox/MainActivity.java
+++ app/src/main/java/com/example/cinebox/MainActivity.java
@@ -10,6 +10,11 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        loadFromBDToMemory();
+    }
 
+    private void loadFromBDToMemory() {
+        SQLiteManager sqLiteManager = SQLiteManager.instanceOfDatabase(this);
+        sqLiteManager.populateLists();
     }
 }
