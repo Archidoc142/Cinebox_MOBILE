48f81ccd80e4979931cfdd3a24dcf2c85a4dfce8
AmelieBergeron <ameber22@gmail.com>
2024-05-16 08:12:19 -0400

    layouts tarifs + panier

---
+++ app/src/main/java/com/example/cinebox/Tarifs.java
@@ -0,0 +1,14 @@
+package com.example.cinebox;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Tarifs extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_tarifs);
+    }
+}
